8ec2df496647db54ff43ab579bd1e4c21e9deef6
lozhang19 <118510433+lozhang19@users.noreply.github.com>
2023-05-09 13:51:12 -0700

    Update script.m

--- 1D/FSH/script.m
+++ 1D/FSH/script.m
@@ -56,37 +56,6 @@ FSH_fixed_point = fsolve(@(FSH) FSH_steady_state(FSH, E2_value, Ih_value, rF), i
 
 disp(['FSH fixed point: ', num2str(FSH_fixed_point)]);
 
-%% Sensitivity
-
-% Vary the rF parameter
-rF_values = linspace(0.5, 3, 10);  % Create a range of rF values to test
-
-% Initialize a matrix to store FSH concentrations
-FSH_values = zeros(length(rF_values), length(t));
-
-% Loop through each rF value
-for i = 1:length(rF_values)
-    % Update the FSH_equation function with the new rF value
-    FSH_equation_new = @(t, FSH) FSH_equation_sensitivity(t, FSH, E2, Ih, rF_values(i));
-    
-    % Run the simulation
-    [t_temp, FSH_temp] = ode45(@(t, FSH) FSH_equation_new(t, FSH), [startTime, stopTime], initialFSH);
-    
-    % Interpolate FSH_temp to match the size of t
-    FSH_temp_interp = interp1(t_temp, FSH_temp, t);
-    
-    % Store the FSH concentrations for the current rF value
-    FSH_values(i, :) = FSH_temp_interp';
-end
-
-% Plot the sensitivity analysis results
-figure;
-surf(t, rF_values, FSH_values);
-xlabel('Time (days)');
-ylabel('rF Parameter');
-zlabel('FSH Concentration');
-title('Sensitivity Analysis: FSH vs rF Parameter');
-
 %% Sensitivity_gammaF
 
 % Vary the rF parameter
@@ -114,41 +83,128 @@ end
 figure;
 surf(t, gammaF_values, FSH_values);
 xlabel('Time (days)');
-ylabel('rF Parameter');
+ylabel('gammaF Parameter');
 zlabel('FSH Concentration');
 title('Sensitivity Analysis: FSH vs gammaF Parameter');
 
-%% Bifurcation
-% Load the FSH_system function
-
-system_name = 'FSH_system';
-
-% Set the parameters for E2 and Ih functions (use the values from your original script)
-E2_value = E2(14);
-Ih_value = Ih(14);
-
-% Define the parameter range for rF
-rF_range = [0.5, 10];
-
-% Initial condition for FSH
-initialFSH = 14;
-
-% Set up the bifurcation analysis
-ap = 1; % index of the bifurcation parameter in the parameter vector
-p = [rF_range(1), E2_value, Ih_value];
-[x0, v0] = init_EP_EP(system_name, initialFSH, p, ap);
-
-% Set the options for the continuation
-opt = contset;
-opt.MaxNumPoints = 100; % number of points in the continuation
-opt.Singularities = 1; % detect singularities
-
-% Perform the bifurcation analysis
-[x, v, s, h, f] = cont(@equilibrium, x0, v0, opt);
-
-% Plot the bifurcation diagram
+% Create a new figure
 figure;
-plot(x(2,:), x(1,:), 'b');
-xlabel('Parameter (rF)');
-ylabel('FSH Steady-State Concentration');
-title('Bifurcation Diagram');
+
+% First subplot: Time vs FSH concentration
+subplot(2, 1, 1);
+for i = 1:length(gammaF_values)
+    plot(t, FSH_values(i, :), 'LineWidth', 2);
+    hold on;
+end
+
+xlabel('Time (days)');
+ylabel('FSH Concentration');
+title('Sensitivity Analysis: Time vs FSH Concentration');
+legend(arrayfun(@(x) sprintf('gammaF = %.2f', x), gammaF_values, 'UniformOutput', false), 'Location', 'bestoutside');
+
+% Second subplot: gammaF vs FSH concentration at specific time points
+subplot(2, 1, 2);
+specific_time_points = [0, 7, 14, 28]; % Set the desired time points
+num_time_points = length(specific_time_points);
+time_idx = zeros(1, num_time_points);
+
+% Find the indices corresponding to the specific time points
+for i = 1:num_time_points
+    [~, time_idx(i)] = min(abs(t - specific_time_points(i)));
+end
+
+% Plot each time point in the same subplot with a unique color and linestyle
+plot_styles = {'-', '-', '-', '-'};
+colors = {'b', 'r', 'g', 'k'};
+for i = 1:num_time_points
+    plot(gammaF_values, FSH_values(:, time_idx(i)), 'LineWidth', 2, ...
+        'Color', colors{i}, 'LineStyle', plot_styles{i});
+    hold on;
+end
+xlabel('gammaF Parameter');
+ylabel('FSH Concentration');
+
+% Create the legend with the time points
+legend_labels = cell(1, num_time_points);
+for i = 1:num_time_points
+    legend_labels{i} = sprintf('t = %.1f days', t(time_idx(i)));
+end
+legend(legend_labels, 'Location', 'best');
+
+% Set the title
+title('Sensitivity Analysis: gammaF vs FSH Concentration at Specific Time Points');
+
+%% Sensitivity with respect to FSH
+
+% Vary the FSH parameter
+FSH_values = linspace(0, 100, 10);  % Create a range of FSH values to test
+
+% Initialize a matrix to store FSH concentrations
+FSH_sensitivity = zeros(length(FSH_values), length(t));
+
+% Loop through each FSH value
+for i = 1:length(FSH_values)
+    % Run the simulation with the new initial FSH value
+    [t_temp, FSH_temp] = ode45(@(t, FSH) FSH_equation(t, FSH, E2, Ih), [startTime, stopTime], FSH_values(i));
+    
+    % Interpolate FSH_temp to match the size of t
+    FSH_temp_interp = interp1(t_temp, FSH_temp, t);
+    
+    % Store the FSH concentrations for the current FSH value
+    FSH_sensitivity(i, :) = FSH_temp_interp';
+end
+
+% Plot the sensitivity analysis results
+figure;
+surf(t, FSH_values, FSH_sensitivity);
+xlabel('Time (days)');
+ylabel('FSH Parameter');
+zlabel('FSH Concentration');
+title('Sensitivity Analysis: FSH vs FSH Parameter');
+
+% Create a new figure
+figure;
+
+% First subplot: Time vs FSH concentration
+subplot(2, 1, 1);
+for i = 1:length(FSH_values)
+    plot(t, FSH_sensitivity(i, :), 'LineWidth', 2);
+    hold on;
+end
+
+xlabel('Time (days)');
+ylabel('FSH Concentration');
+title('Sensitivity Analysis: Time vs FSH Concentration');
+legend(arrayfun(@(x) sprintf('FSH = %.2f', x), FSH_values, 'UniformOutput', false), 'Location', 'bestoutside');
+
+% Second subplot: FSH vs FSH concentration at specific time points
+subplot(2, 1, 2);
+specific_time_points = [0, 7, 14, 28]; % Set the desired time points
+num_time_points = length(specific_time_points);
+time_idx = zeros(1, num_time_points);
+
+% Find the indices corresponding to the specific time points
+for i = 1:num_time_points
+    [~, time_idx(i)] = min(abs(t - specific_time_points(i)));
+end
+
+% Plot each time point in the same subplot with a unique color and linestyle
+plot_styles = {'-', '-', '-', '-'};
+colors = {'b', 'r', 'g', 'k'};
+for i = 1:num_time_points
+    plot(FSH_values, FSH_sensitivity(:, time_idx(i)), 'LineWidth', 2, ...
+        'Color', colors{i}, 'LineStyle', plot_styles{i});
+    hold on;
+end
+xlabel('FSH Parameter');
+ylabel('FSH Concentration');
+
+% Create the legend with the time points
+legend_labels = cell(1, num_time_points);
+for i = 1:num_time_points
+    legend_labels{i} = sprintf('t = %.1f days', t(time_idx(i)));
+end
+legend(legend_labels, 'Location', 'best');
+
+% Set the title
+title('Sensitivity Analysis: FSH vs FSH Concentration at Specific Time Points');

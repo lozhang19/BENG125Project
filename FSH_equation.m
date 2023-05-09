2a4d510402313169220df19f46d58015d6fa7cce
lozhang19 <118510433+lozhang19@users.noreply.github.com>
2023-05-09 09:57:16 -0700

    Delete FSH_equation.m

--- FSH_equation.m
+++
@@ -1,9 +0,0 @@
-function dFSHdt = FSH_equation(t, FSH, E2, Ih, rF)
-    % Parameters
-    dF = 0.4;
-    alphaF = 0.3;
-    gammaF = 0.1;
-
-    % Equation
-    dFSHdt = rF * (1 + alphaF * E2(t)) * (1 - (Ih(t)/(gammaF + Ih(t)))) - dF * FSH;
-end

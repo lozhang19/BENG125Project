54cef14b9d5af7acb27af06d7b5bb083cab50a54
lozhang19 <118510433+lozhang19@users.noreply.github.com>
2023-05-09 09:57:33 -0700

    Delete FSH_steady_state.m

--- FSH_steady_state.m
+++
@@ -1,9 +0,0 @@
-function dFSHdt_ss = FSH_steady_state(FSH, E2, Ih, rF)
-    % Parameters
-    dF = 0.4;
-    alphaF = 0.3;
-    gammaF = 0.1;
-
-    % Steady-state equation
-    dFSHdt_ss = rF * (1 + alphaF * E2) * (1 - (Ih / (gammaF + Ih))) - dF * FSH;
-end
